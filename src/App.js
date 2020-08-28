0de02ff00911d9d51c933e9cd0282d5954979488
Sat Naing <satnaingdev@gmail.com>
2020-08-28 21:53:39 +0630

    Finish header component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./header.scss";
+
+const Header = () => (
+  <div className="header">
+    <NavLink className="logo-container" exact activeCLassName="active-nav" to="/">
+      <Logo />
+    </NavLink>
+    <div className="options">
+      <NavLink className="option" exact activeCLassName="active-nav" to="/shop">
+        Shop
+      </NavLink>
+      <NavLink className="option" exact activeCLassName="active-nav" to="/contact">
+        Contact
+      </NavLink>
+    </div>
+  </div>
+);
+
+export default Header;
